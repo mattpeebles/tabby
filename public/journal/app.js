93bb4b0733ef19893fe711e7a57f436574ccf430
Matthew Peebles <peebles.mm@gmail.com>
2017-07-08 15:29:44 -0700

    Added post functionality

--- public/journal/app.js
+++ public/journal/app.js
@@ -35,32 +35,95 @@ const MOCK_JOURNAL_ENTRIES = {
 	]
 }
 
-function getJournalEntries(callback){
-	setTimeout(() => {callback(MOCK_JOURNAL_ENTRIES)}, 100)
-}
 
-function displayJournalEntries(data){
-	for (index in data.journalEntries) {
-		let entry = data.journalEntries[index]
-		if (entry.priority == 'high'){
-			$('#highPriority').append(
-				'<p>' + '<a href=\"' + entry.link + '\">' + entry.title + '<a/></p>')
-		}
-		else if(entry.priority == 'medium'){
-			$('#medPriority').append(
-				'<p>' + '<a href=\"' + entry.link + '\">' + entry.title + '<a/></p>')
-		}
-		else if(entry.priority == 'low'){
-			$('#lowPriority').append(
-				'<p>' + '<a href=\"' + entry.link + '\">' + entry.title + '<a/></p>')
+	// Get journal entries
+// *********************************** //
+	function getJournalEntries(callback){
+		setTimeout(() => {callback(MOCK_JOURNAL_ENTRIES)}, 100)
+	}
+
+	function displayJournalEntries(data){
+		for (index in data.journalEntries) {
+			let entry = data.journalEntries[index]
+			if (entry.priority == 'high'){
+				$('#highPriority').append(
+					'<p>' + '<a href=\"' + entry.link + '\">' + entry.title + '<a/></p>')
+			}
+			else if(entry.priority == 'medium'){
+				$('#medPriority').append(
+					'<p>' + '<a href=\"' + entry.link + '\">' + entry.title + '<a/></p>')
+			}
+			else if(entry.priority == 'low'){
+				$('#lowPriority').append(
+					'<p>' + '<a href=\"' + entry.link + '\">' + entry.title + '<a/></p>')
+			}
 		}
 	}
-}
 
-function getAndDisplayJournalEntries(){
-	getJournalEntries(displayJournalEntries)
-}
+	function getAndDisplayJournalEntries(){
+		getJournalEntries(displayJournalEntries)
+	}
+// *********************************** //
+
+
+	// Mock post journal entries
+// *********************************** //
+	function addJournalEntryForm(){
+		let formTemplate = "<div id=\"newLinkFormDiv\">" +
+								"<form id=\"newLinkform\">" +
+									"<label>Title</label>" +
+									"<input type=\"text\" name=\"title\" placeholder=\"Title\" id=\"linkTitle\"></input>" +
+									"<label>Link</label>" +
+									"<input type=\"text\" name=\"link\" placeholder=\"www.google.com\" id=\"linkUrl\"></input>" +
+									"<label>Priority</label>" +
+									"<select name=\"priority\" id=\"linkPriority\">" +
+										"<option value=\"high\">High</option>" +
+										"<option value=\"medium\">Medium</option>" +
+										"<option value=\"low\">Low</option>" +
+									"<input type=\"submit\" name=\"submit\" id=\"newLinkFormSubmit\"></input>" +
+								"<form>" +
+							"</div>";
+
+		$('#newLink').on('click', () => {
+			$('#linkSection').prepend(formTemplate)
+		})
+	}
+
+	function postJournalEntry(){
+		$("#linkSection").on('click', '#newLinkFormSubmit', (event) => {
+			event.preventDefault();
+
+			let title = $('#linkTitle').val()
+			let url = $('#linkUrl').val()
+			let priority = $('#linkPriority').val()
+			let randId = Math.floor(100000 + Math.random() * 900000)
+
+			let newLink = {
+				'id': randId,
+				'title': title,
+				'priority': priority,
+				'link': url,
+				'user': 'john doe',
+				'date': Date.now
+			}
+
+
+			MOCK_JOURNAL_ENTRIES["journalEntries"].push(newLink)
+
+			getAndDisplayJournalEntries()
+
+			$('#newLinkFormDiv').remove()
+
+
+		})
+	}
+// *********************************** //
+
+
+
 
 $(() => {
 	getAndDisplayJournalEntries()
+	addJournalEntryForm()
+	postJournalEntry()
 })
