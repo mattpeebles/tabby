5dce733ba129e92da91004cf293e9dc4b4c05f78
Matthew Peebles <peebles.mm@gmail.com>
2017-07-13 18:01:08 -0700

    Added test database url for TDD

--- config.js
+++ config.js
@@ -2,4 +2,7 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
 				global.DATABASE_URL ||
 				'mongodb://localhost/sophon' //needs to match database name (obv)
 
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
+							'mongodb://localhost/test-sophon'
+
 exports.PORT = process.env.PORT || 3030
