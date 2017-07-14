2b248b36418ab87feea87f0f5ca8a87a8c20fb5c
Matthew Peebles <peebles.mm@gmail.com>
2017-07-14 12:51:47 -0700

    Added entrySchmea and added journalId to userschema
    Instead of including journal entries in the User object. Split
    journal entries into a seperate collection with a unique journal
    id for each user. Added journalid to userschema so that it will be easy to identify. This allow for easy modelling of the data, and a separate api resource for the journal entries.

--- models.js
+++ models.js
@@ -8,7 +8,7 @@ const userSchema = mongoose.Schema({
 	expiry: {type: Date},
 	email: {type: String, required: true},
 	password: {type: String, required: true},
-	entries: {type: Array},
+	journalId: {type: String}, //will be unique string that identifies the journal entries in seperate collection
 	priorityExpiry: {type: Object}
 })
 
@@ -22,13 +22,37 @@ userSchema.methods.userRepr = function(){
 		id: this._id,
 		user: this.fullName,
 		email: this.email,
-		entries: this.entries,
+		journalId: this.journalId,
 		joinDate: this.joinDate.toString(),
 		expiry: this.expiry,
 		priorityExpiry: this.priorityExpiry
 	}
 }
 
+const entrySchema = mongoose.Schema({
+	journalId: {type: String, required: true},
+	entryId: {type: String, required: true},
+	title: {type: String},
+	link: {type: String},
+	priority: {type: String, required: true},
+	addDate: {type: Date},
+	expiry: {type: Date}
+})
+
+entrySchema.methods.entryRepr = function(){
+	return {
+		journalId: this.journalId,
+		entryId: this._id,
+		title: this.title,
+		link: this.link,
+		prioirty: this.priority,
+		addDate: this.addDate,
+		expiry: this.expiry
+	}
+}
+
 const Users = mongoose.model('users', userSchema) //first option must match collection name
+const Entry = mongoose.model('entries', entrySchema)
+
 
 module.exports = {Users}
