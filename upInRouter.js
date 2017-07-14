0e1b56a9f1c2de4ca4169bebc28a76879ac88b39
Matthew Peebles <peebles.mm@gmail.com>
2017-07-14 11:18:54 -0700

    Added post api hook

--- upInRouter.js
+++ upInRouter.js
@@ -26,4 +26,32 @@ upInRouter.get("/", (req, res) => {
 			})
 })
 
+upInRouter.post("/", jsonParser, (req, res) => {
+	const requiredKeys = ['firstName', 'lastName', 'email', 'password']
+	requiredKeys.forEach(key => {
+		if (!(key in req.body)){
+			const message = `Missing ${key} in request body`
+			console.error(message)
+			return res.status(400).send(message)
+		}
+	})
+	
+	Users
+		.create({
+			user: {	firstName: req.body.firstName,
+					lastName: req.body.lastName
+				},
+			email: req.body.email,
+			password: req.body.password,
+			joinDate: req.body.joinDate || Date.now(),
+			entries: [],
+		})
+		.then(
+			user => res.status(201).json(user.userRepr()))
+		.catch(err => {
+			console.error(err)
+			res.status(500).json({message: 'Internal server error'})
+		})
+})
+
 module.exports = upInRouter
