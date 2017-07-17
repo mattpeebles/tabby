011b6fa597e5a83b9ce96b311fbe34047eb9f473
Matthew Peebles <peebles.mm@gmail.com>
2017-07-16 17:29:22 -0700

    Added PUT api hook

--- entryRouter.js
+++ entryRouter.js
@@ -10,7 +10,10 @@ const jsonParser = bodyParser.json()
 
 entryRouter.use(jsonParser)
 
-const {Entry} = require('./models')
+const {Users, Entry} = require('./models')
+
+const {addDays} = require('./resources/date-module')
+
 
 entryRouter.get('/', (req, res) => {
 	Entry
@@ -50,7 +53,9 @@ entryRouter.get('/:journalId', (req, res) => {
 
 entryRouter.post('/', (req, res) => {
 	const requiredFields = ['title', 'link', 'priority', 'journalId'];
-	
+	let priorityExpiryObject = {}
+	let priority = req.body.priority
+	let addDate = new Date(Date(req.body.addDate))
 	requiredFields.forEach((field) => {
 		if (!(field in req.body)) {
 			const message = `Missing ${field} in request body`;
@@ -59,21 +64,100 @@ entryRouter.post('/', (req, res) => {
 		}
 	})
 
+		Users
+			.find({journalId: req.body.journalId})
+			.exec()
+			.then(res => {
+				priorityExpiryObject = res[0].priorityExpiry
+				expiry = addDays(addDate, priorityExpiryObject[priority])
+				return expiry
+			})
+			.then(expiry => {
+				Entry
+							.create({
+								title: req.body.title,
+								link: req.body.entry,
+								journalId: req.body.journalId,
+								priority: req.body.priority,
+								addDate: req.body.addDate,
+								expiry: expiry
+							})
+							.then(entry => res.status(201).json(entry.entryRepr()))
+							.catch(err => {
+								console.error(err)
+								return res.status(400).json({message: 'Internal server error'})
+							})
+			})
+})
 
+entryRouter.put('/:entryId', (req, res) => {
+	if (!(req.params.entryId === req.body.entryId)){
+		const message = (
+		  `Request path entryId (${req.params.id}) and request body entryId ` +
+		  `(${req.body.id}) must match`);
+		console.error(message);
+		res.status(400).json({message: message});
+	}
+
+	let toUpdate = {}
+	const updateableFields = ['title', 'link', "priority"]
+	let addDate;
+	
+	updateableFields.forEach(field => {
+		if(field in req.body){
+			toUpdate[field] = req.body[field]
+		}
+	})
+
+	if (!("priority" in toUpdate)){
 		Entry
-			.create({
-				title: req.body.title,
-				link: req.body.entry,
-				journalId: req.body.journalId,
-				priority: req.body.priority,
-				addDate: req.body.addDate,
-				expiry: req.body.expiry
+			.findByIdAndUpdate(req.params.entryId, {$set: toUpdate}, {new: true})
+			.then(updateEntry => res.status(201).json(updateEntry.entryRepr()))
+	}
+	else {
+		Entry
+			.findById(req.body.entryId)
+			.exec()
+			.then(res => {
+				let journalId = res.journalId
+				return journalId
 			})
-			.then(entry => res.status(201).json(entry.entryRepr()))
-			.catch(err => {
-				console.error(err)
-				return res.status(400).json({message: 'Internal server error'})
+			.then(journalId => {
+				Users
+					.find({journalId: journalId})
+					.exec()
+					.then(res => {
+						let priorityExpiryObject = res[0].priorityExpiry
+						return priorityExpiryObject
+					})
+					.then(object => {
+						let priority = req.body.priority
+						let priorityExpiry = object[priority]
+						console.log("PRIORITY EXPIRY: " + priorityExpiry)
+						return priorityExpiry
+					})
+					.then(priorityExpiry => {
+						Entry
+							.find({entryId: req.params.entryId})
+							.exec()
+							.then(res => {
+								addDate = new Date(Date(res.addDate))
+								expiry = addDays(addDate, priorityExpiry)
+								console.log("ADD DATE: " + addDate)
+								console.log("EXPIRY: " + expiry)
+								return expiry 
+							})
+							.then(expiry => {
+								toUpdate.expiry = expiry
+								Entry
+									.findByIdAndUpdate(req.params.entryId, {$set: toUpdate}, {new: true})
+									.then(updateEntry => res.status(201).json(updateEntry.entryRepr()))
+									.catch(err => res.status(500).message({message: 'Internal server error'}))
+							})
+					
+					})
 			})
+	}
 })
 
 
