d9a5f01257c82f7ba4fa7155348d6eff022d3248
Matthew Peebles <peebles.mm@gmail.com>
2017-07-29 18:12:49 -0700

    Removed add form function and cleaned css and index

--- public/profile/app-brow.js
+++ public/profile/app-brow.js
@@ -39,7 +39,7 @@ RandomCat.prototype.get = function (options) {
 module.exports = (new RandomCat());
 },{}],2:[function(require,module,exports){
 const randomCat = require('random-cat')
-const DATABASE_URL = "http://localhost:3030/users"
+const DATABASE_URL = "http://localhost:3030"
 
 
 
@@ -47,7 +47,17 @@ const DATABASE_URL = "http://localhost:3030/users"
 // *********************************** //
 
 	function getUserData(callback){
-		$.get(DATABASE_URL + '/me', callback)
+		$.ajax({
+			type: 'get',
+			url: DATABASE_URL + '/users/me',
+			success: function(data){
+				callback(data)
+			},
+			error: function(err){
+				alert('please log in')
+				window.location.href = DATABASE_URL + '/login'
+			}
+		})
 	}
 
 	function displayUserData(data){
@@ -59,26 +69,6 @@ const DATABASE_URL = "http://localhost:3030/users"
 
 		})
 		let formatDate = $.format.date(data.user.joinDate, "MMMM D, yyyy")
-		
-		// let userHTML = 	`<div id="userName">` + 
-		// 					`<p>Name: <span id="firstName">${data.user.user.firstName} ${data.user.user.lastName}</span></p>` +
-		// 				`</div>` +
-		// 				`<div id="userJoinDate">` +
-		// 					`<p>Join Date: <span id="joinDate">${formatDate}</span></p>` +
-		// 				`</div>` +
-		// 				`<div id="userEmail">` +
-		// 					`<p>Email: <span id="email">${data.user.email}</span></p>` +
-		// 				`</div>` +
-		// 				`<div id="userPriorityExpiry">` +
-		// 					`<p>Priority Settings</p>` +
-		// 					`<div id='priorities'>` +
-		// 						`<p id="priorityHigh">High: <span id="high">${data.user.priorityExpiry.high}</span></p>` +
-		// 						`<p id="priorityMedium">Medium: <span id="medium">${data.user.priorityExpiry.medium}</span></p>` +
-		// 						`<p id="priorityLow">Low: <span id="low">${data.user.priorityExpiry.low}</span></p>` +
-		// 					`</div>` +
-		// 				`</div>`;
-
-		// $('#userInfo').append(userHTML)
 
 		$('#profileImage').append(`<img src="${catPhoto}"></img>`)
 		$("#name").append(`<p>${data.user.user.firstName} ${data.user.user.lastName}</p>`)
@@ -94,121 +84,27 @@ const DATABASE_URL = "http://localhost:3030/users"
 	}
 // *********************************** //
 
-
-
-	// Edit User Data
+	// Signout
 // *********************************** //
-	function editUserData(data){
-
-		let editForm = 	
-			`<form class='horizontal' id='editUserForm'>` +
-				`<div class='form-group' id='firstName'>`+
-					`<label for='firstName' class='col-sm-2' control-label>First Name: </label>` +
-					`<div class='col-sm-10'>` +
-						`<input type="text" name="firstName" class='form-control' placeholder="${data.user.user.firstName}" id="firstName"></input>` +
-					`</div>` +	
-				`</div>` +
-				`<div class='form-group' id='lastName'>` +
-					`<label class='col-sm-2' for="lastName" control-label>Last Name: </label>` +
-					`<div class='col-sm-10'>` +
-						`<input type="text" name="lastName" class='form-control' placeholder="${data.user.user.lastName}" id="firstName"></input>` +
-					`</div>` +	
-				`</div>` +
-				`<div class='form-group' id="userEmail">` +
-					`<label for="email" class='col-sm-2' control-label>Email: </label>`+
-					`<div class='col-sm-10'>` +	
-						`<input type="text" name="email" class='form-control' placeholder="${data.user.email}" id="email"></input>` +
-					`</div>` +	
-				`</div>` +
-				`<div id="userPriorityExpiry">` +
-					`<p>Priority Settings</p>` +
-					`<div class="form-group priorities">` +
-						`<label for="high" class='col-sm-2' control-label>High: </label>`+
-						`<div class='col-sm-10'>` +
-							`<input type="text" name="high" class='form-control' placeholder="${data.user.priorityExpiry.high}" id="high"></input>`+
-						`</div>` +	
-					`<div class='form-group priorities'>` +
-						`<label for="medium" class='col-sm-2' control-label>Medium: </label>`+
-						`<div class='col-sm-10'>` +
-							`<input type="text" name="medium" class='form-control' placeholder="${data.user.priorityExpiry.medium}" id="medium"></input>`+	
-						`</div>` +
-					`</div>` +		
-					`<div class='form-group priorities'>` +
-						`<label for="low" class='col-sm-2' control-label>Low: </label>`+
-						`<div class='col-sm-10'>` +
-							`<input type="text" name="low" class='form-control' placeholder="${data.user.priorityExpiry.low}" id="low"></input>`+
-						`</div>` +
-					`</div>` +		
-				`</div>`+
-				`<div class='form-group' id='buttonContainer'>` +
-					`<div class='col-sm-offset-2 col-sm-10' id='buttonDiv'>` +
-						`<input class='btn btn-default' type="submit" name="submit" id="editUserSubmit"></input>` +
-					`</div>` +
-				`</div>` +
-				`<button type="cancel"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>` +
-			`</form>`;
-
-		$('#userSection').append(editForm)
-
-	}
-
-	function getAndEditUserData(){
-		$('#editUserButton').on('click', (event) => {
-			
-			$('#userSection').empty()
-			getUserData(editUserData)
-
-			$('#userSection').on('click', '#editUserSubmit', (event) => {
-				event.preventDefault()
-				let user = {};
-				let priorityExpiry = {};
-
-					//serializes object to database standard if input has a string
-				let userData = $('#editUserForm').serializeArray().reduce((obj, item) => {
-			   		 if((item.name === "firstName" || item.name === "lastName") && item.value !== ""){
-			   		 	user[item.name] = item.value
-			   		 }
-			   		 else if((item.name === 'high' || item.name === 'medium' || item.name === 'low') && item.value !== ""){
-			   		 	priorityExpiry[item.name] = parseInt(item.value) //parseInt is required to ensure expiry is a number.
-			   		 }
-			   		 else if(item.value !== ""){
-			   		 	obj[item.name] = item.value;
-			   		}
-
-
-			   			//adds user and priorityexpiry object to returned object
-			   			//if it is not empty
-			   		if(Object.keys(user).length !== 0 && user.constructor === Object){
-				   		obj["user"] = user
-			   		}
-			   		if(Object.keys(priorityExpiry).length !== 0 && priorityExpiry.constructor === Object){
-						obj["priorityExpiry"] = priorityExpiry
-			   		}
-
-			   		 return obj;
-					}, {});
-
-
-				getUserData((data) => {
-					userData.id = data.user.id
-					
-					$.ajax({
-						type: 'put',
-						url: DATABASE_URL + '/' + data.user.id,
-						data: JSON.stringify(userData),
-						contentType: 'application/json',
-						success: function(){
-							location.reload()
-						}
-					})
+	function signout(){
+		console.log('yo')
+		$('#logOutButton').click((event) => {
+			console.log('hello')
+			if(confirm('Log out?')){
+				$.ajax({
+					type: 'get',
+					url: 'http://localhost:3030/logout',
+					success: function(data) {
+						alert(data.message)
+						window.location.href = data.redirect
+					}
 				})
-			})
+			}
 		})
 	}
-// *********************************** //
 
 $(() => {
 	getAndDisplayUserData()
-	getAndEditUserData()
+	signout()
 })
 },{"random-cat":1}]},{},[2]);
